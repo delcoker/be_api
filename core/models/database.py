6f8ed255032a982c4e1f8af1c5353b599766c9d4
Jude <j_norvor@hotmail.com>
2021-03-06 14:20:47 +0000

    clearing up db file

--- core/models/database.py
+++ core/models/database.py
@@ -7,10 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:''@127.0.0.1[:3306]/user_management"
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@127.0.0.1:3306/user_management"
 SQLALCHEMY_DATABASE_URL = os.getenv('MYSQLURLPATH')
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
