ea07c125673d766f0ece51cfb5f7ba1a64b3532f
Jude <j_norvor@hotmail.com>
2021-05-02 21:02:26 +0000

    Create keywords.py

---
+++ core/schemas/keywords.py
@@ -0,0 +1,18 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+class KeywordBase(BaseModel):
+    keywords: str
+
+class KeywordCreate(KeywordBase):
+    category_id: int
+
+    class Config:
+        orm_mode = True
+
+class Keyword(KeywordCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
