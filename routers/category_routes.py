7b70c3986424d4914989f871bff0134f3a0d92ce
Jude <j_norvor@hotmail.com>
2021-05-09 12:47:32 +0000

    code cleanup on category routes

--- routers/category_routes.py
+++ routers/category_routes.py
@@ -31,7 +31,7 @@ def get_all_categories(req: Request, db: Session = Depends(get_db)):
 
 # Route to create a category
 @router.post("/create")
-def create_category(req: Request, category_name: str = Form(...), group_category_id: int = Form(...), keywords: str = Form(...), db: Session = Depends(get_db)):
+def create_category(category_name: str = Form(...), group_category_id: int = Form(...), keywords: str = Form(...), db: Session = Depends(get_db)):
     category_controller.create_category(db, category_name, group_category_id, keywords)
     return {"message": "Category created succesfully"}
 
@@ -59,21 +59,3 @@ def delete_category(category_id: int, db: Session = Depends(get_db)):
     db_category = category_controller.delete_category(db, category_id)
     if db_category == 1:
         return {"message": "Category has been deleted succesfully"}
-
-# # , response_model=group_categories.GroupCategory
-# @router.post("/group/category/update/{group_category_id}")
-# def update_group_category(group_category_id: int, group_category: group_categories.GroupCategoryCreate, db: Session = Depends(get_db)):
-#     db_group_category = crud.update_group_category(
-#         db, group_category_id, group_category)
-#     if db_group_category is None:
-#         raise HTTPException(status_code=404, detail="Group Category not found")
-#     return {"message": "Group Category has been updated succesfully"}
-
-# # Delete specified group category
-
-
-# @router.post("/group/category/delete{group_category_id}")
-# def delete_group_category(group_category_id: int, db: Session = Depends(get_db)):
-#     db_group_category = crud.delete_group_category(db, group_category_id)
-#     if db_group_category == 1:
-#         return {"message": "Group Category has been deleted succesfully"}
