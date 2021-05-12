bb0a3026dc7335c1c35271579901727192bd0030
Jude <j_norvor@hotmail.com>
2021-05-12 21:47:44 +0000

    started setting up alembic

---
+++ alembic/versions/28b495605c74_create_user_table.py
@@ -0,0 +1,24 @@
+"""create user table
+
+Revision ID: 28b495605c74
+Revises: 
+Create Date: 2021-05-12 21:43:17.194681
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '28b495605c74'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
