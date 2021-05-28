a1a7185e3f398b91cd03ffaaea0d57534855d604
Jude <j_norvor@hotmail.com>
2021-05-28 20:08:56 +0000

    keep stream alive fix - thanks to Kingston

--- controllers/streams_controller.py
+++ controllers/streams_controller.py
@@ -1,7 +1,7 @@
 # System Imports
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from fastapi_sqlalchemy import db 
+from fastapi_sqlalchemy import db
 
 # Test for stream
 import requests
@@ -21,8 +21,12 @@ from controllers.crud import get_current_user
 from queue import Queue
 import threading
 from dependency.vader_sentiment_api import SentimentApi
+from requests.exceptions import ProxyError, Timeout, ConnectionError, ChunkedEncodingError
+import time
 
 load_dotenv()
+
+
 class MyTwitter:
 
     # Code for generating bearer token
@@ -42,7 +46,7 @@ class MyTwitter:
 
         return data
 
-    def  __init__(self) -> None:
+    def __init__(self) -> None:
         # Start queues for streams and sentiment scores
         self.stream_queue = Queue()
         self.sentiment_queue = Queue()
@@ -52,7 +56,7 @@ class MyTwitter:
         threading.Thread(target=self.score_sentiment, daemon=True).start()
 
         # create headers
-        headers = self.create_headers(os.getenv('BEARER_TOKEN_KINGSTON'))
+        headers = self.create_headers(os.getenv('BEARER_TOKEN_SHAMIR'))
         # get rules
         rules = self.get_rules(headers)
         # delete rules
@@ -62,9 +66,8 @@ class MyTwitter:
         # start stream
         self.get_stream(headers)
 
-
     # Code for creating headers to connect to twitter for the streams
-    def create_headers(self,bearer_token):
+    def create_headers(self, bearer_token):
         headers = {"Authorization": "Bearer {}".format(bearer_token)}
         return headers
 
@@ -102,7 +105,7 @@ class MyTwitter:
     # Code for setting the rules needed by twitter to start the fetch
     def set_rules(self, headers):
         with db():
-            scopes =  db.session.query(users.Scope).all()
+            scopes = db.session.query(users.Scope).all()
             # Put scopes in map to group users with same scopes
             scope_map = self.get_scopes_map(scopes)
             sample_rules = []
@@ -121,61 +124,70 @@ class MyTwitter:
                 )
             print(json.dumps(response.json()))
 
-    def get_scopes_map(self,scopes):
+    def get_scopes_map(self, scopes):
         scope_map = {}
         for scope_row in scopes:
-            scopes_obj = scope_row.scope.split(',') # split scopes
-            for scope in scopes_obj: 
-                sanitized_scope = scope.strip() # strip any leading or trailing whitespaces
+            scopes_obj = scope_row.scope.split(',')  # split scopes
+            for scope in scopes_obj:
+                sanitized_scope = scope.strip()  # strip any leading or trailing whitespaces
                 if sanitized_scope in scope_map:
-                    scope_map[sanitized_scope] = scope_map[sanitized_scope] + "," + str(scope_row.user_id) #if map has scope just concat user
+                    scope_map[sanitized_scope] = scope_map[sanitized_scope] + "," + str(
+                        scope_row.user_id)  # if map has scope just concat user
                     # print(scope_map[sanitized_scope])
                 else:
-                    scope_map[sanitized_scope] = str(scope_row.user_id) # else create an index for the scope with the user id
-        return scope_map # return scope map
+                    scope_map[sanitized_scope] = str(
+                        scope_row.user_id)  # else create an index for the scope with the user id
+        return scope_map  # return scope map
 
     # Start getting tweets that contain the rules specified
-    def get_stream(self,headers): #, token:str set, bearer_token, 
+    def get_stream(self, headers):  # , token:str set, bearer_token,
         print("getting streams method")
-        response = requests.get(
-            "https://api.twitter.com/2/tweets/search/stream?tweet.fields=attachments,author_id,created_at,entities,id,lang,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,text,withheld&expansions=author_id,geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type", headers=headers, stream=True,
-        )
-        # print(response.status_code)
-        if response.status_code != 200:
-            raise Exception(
-                "Cannot get stream (HTTP {}): {}".format(
-                    response.status_code, response.text
-                )
+        count = 120
+        while True:
+            response = requests.get(
+                "https://api.twitter.com/2/tweets/search/stream?tweet.fields=attachments,author_id,created_at,entities,id,lang,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,text,withheld&expansions=author_id,geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type",
+                headers=headers, stream=True,
             )
-        for response_line in response.iter_lines():
-            if response_line:
-                json_response = json.loads(response_line)
-                self.stream_queue.put(json_response)
+            try:
+                for response_line in response.iter_lines():
+                    if response_line:
+                        json_response = json.loads(response_line)
+                        self.stream_queue.put(json_response)
+                        count = 120
+            except Exception:
+                if response.status_code == 429:
+                    time.sleep(count)
+                    count *= count
+                else:
+                    print(' Put to sleep before retrying.')
+                    time.sleep(5)
+                    print("Printed after 5 seconds.")
+                continue
 
     def score_sentiment(self):
         print("score sentiment method")
         while True:
-                post_to_score = self.sentiment_queue.get()
-                if post_to_score:
-                    print(f"Scoring {post_to_score.id}")
-                    result = SentimentApi().getSentiment(str(post_to_score.text))
+            post_to_score = self.sentiment_queue.get()
+            if post_to_score:
+                print(f"Scoring {post_to_score.id}")
+                result = SentimentApi().getSentiment(str(post_to_score.text))
 
-                    db_sentiment = users.PostSentimentScore(
-                        post_id=post_to_score.id, 
-                        sentiment=result["sentiment"], 
-                        score=result["score"]
-                    )
-                    try:
-                        with db():
-                            db.session.add(db_sentiment)
-                            db.session.commit()
-                            db.session.refresh(db_sentiment)
-                    except Exception as e:
-                        # print("NOT saved")
-                        print(e)
-                    print(f"Scored {post_to_score.id}")
+                db_sentiment = users.PostSentimentScore(
+                    post_id=post_to_score.id,
+                    sentiment=result["sentiment"],
+                    score=result["score"]
+                )
+                try:
+                    with db():
+                        db.session.add(db_sentiment)
+                        db.session.commit()
+                        db.session.refresh(db_sentiment)
+                except Exception as e:
+                    # print("NOT saved")
+                    print(e)
+                print(f"Scored {post_to_score.id}")
 
-    def store_streams(self): #, token: str, db: Session = Depends(get_db)
+    def store_streams(self):  # , token: str, db: Session = Depends(get_db)
         print("store streams method")
         while True:
             stream_results = self.stream_queue.get()
@@ -188,17 +200,17 @@ class MyTwitter:
                     geo_location = ''
                 # Split user ids that are returned from twitter
                 user_ids = stream_results['matching_rules'][0]["tag"].split(",")
-                for user_id in user_ids: 
+                for user_id in user_ids:
                     db_stream = users.Post(
-                        user_id = user_id,
-                        source_name = "twitter",
-                        data_id = stream_results["data"]["id"],
-                        data_author_id = stream_results["data"]["author_id"],
-                        data_user_name = stream_results["includes"]["users"][0]["username"],
-                        data_user_location = geo_location,
-                        text = stream_results["data"]["text"],
-                        full_object = json.dumps(stream_results, indent=4, sort_keys=True),
-                        created_at = stream_results["data"]["created_at"]
+                        user_id=user_id,
+                        source_name="twitter",
+                        data_id=stream_results["data"]["id"],
+                        data_author_id=stream_results["data"]["author_id"],
+                        data_user_name=stream_results["includes"]["users"][0]["username"],
+                        data_user_location=geo_location,
+                        text=stream_results["data"]["text"],
+                        full_object=json.dumps(stream_results, indent=4, sort_keys=True),
+                        created_at=stream_results["data"]["created_at"]
                     )
                     try:
                         with db():
